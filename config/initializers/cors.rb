6c967a3baa1a08a81f4d441d356d82b2b0116c44
Rykelme Steven <rykelmesteven@outlook.com>
2025-03-17 01:21:47 -0300

    allow cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,5 +5,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
                headers: :any,
                methods: %i[get post put patch delete options head]
     end
+    allow do
+      origins 'https://manegement-project.vercel.app'
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
 end
   
